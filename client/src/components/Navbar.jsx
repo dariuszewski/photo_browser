eeb9501b20e22fcbd3d8a8c4b16e153bfdb5eb36
dariuszewski <dariuszewski32@gmail.com>
2025-01-23 21:49:03 +0100

    01_frontend

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import '../css/Navbar.css';
+import { Link } from "react-router-dom";
+
+
+function NavBar() {
+    return (
+        <nav className="navbar">
+            <div className="navbar-brand">
+                <Link to="/">Photo Browser</Link>
+            </div>
+            <div className="navbar-links">
+                <Link to="/">Home</Link>
+                <Link to="/favourites">Favourites</Link>
+            </div>
+        </nav>
+    )
+}
+
+
+export default NavBar;
---
+++ client/src/pages/Favourites.jsx
@@ -0,0 +1,13 @@
+import '../css/Favourites.css';
+
+
+function Favourites() {
+    return (
+        <div className="favourites-empty">
+            <h2>No favourite movies yet</h2>
+            <p>Start adding favourites and they will appear here...</p>
+        </div>
+    );
+}
+
+export default Favourites;
